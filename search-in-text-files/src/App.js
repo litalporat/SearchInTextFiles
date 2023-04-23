6c1830a6b6e3b21bba7ff199c55363d18c4bd239
Lital Porat <poratlital@gmail.com>
2023-04-23 11:42:44 +0300

    textFilesAdded

--- search-in-text-files/src/App.js
+++ search-in-text-files/src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
-import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
-import { margin } from "@mui/system";
+import { useState } from "react";
 
 function App() {
+  const [searchesNum, setSearchesNum] = useState(0);
   return (
     <div className="App">
       <div className="header">Search In The Text Files</div>
-      <Container maxWidth="lg" fixed sx={{ margin: 10 }}>
-        <TextField id="outlined-basic" variant="outlined" />
-      </Container>
+      <div className="container">
+        <div className="row">
+          <span className="left">
+            <strong>Search: </strong>
+            <TextField id="outlined-basic" variant="outlined" />
+          </span>
+          <span className="right">
+            <p>Number of searches</p>
+            <p className="searchesNum">{searchesNum}</p>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
