4c5ef3d575801c6c79174cbd2b23fc413cc5f4b8
Lital Porat <poratlital@gmail.com>
2023-04-23 12:37:49 +0300

    fronend-beginning

--- search-in-text-files/src/App.js
+++ search-in-text-files/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import Divider from "@mui/material/Divider";
 import { useState } from "react";
 
 function App() {
@@ -7,17 +9,21 @@ function App() {
   return (
     <div className="App">
       <div className="header">Search In The Text Files</div>
-      <div className="container">
+      <div className="grid-container">
         <div className="row">
           <span className="left">
             <strong>Search: </strong>
-            <TextField id="outlined-basic" variant="outlined" />
+            <TextField id="outlined-basic" variant="filled" />
           </span>
           <span className="right">
             <p>Number of searches</p>
             <p className="searchesNum">{searchesNum}</p>
           </span>
         </div>
+        <Card variant="outlined" sx={{ textAlign: "center", height: 400 }}>
+          <p>Text Files Found:</p>
+          <Divider />
+        </Card>
       </div>
     </div>
   );
